73bf67f6ada48a66c2d4745420f6aa875649f5bb
Ahmed Khaled <ahmed23121998@gmail.com>
2025-12-31 14:13:59 +0200

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,59 +1,26 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { AddUserComponent } from './pages/users/add-user.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: '',
-    component: LayoutComponent,
+  { path: '', component: LayoutComponent,
     children: [
-      {
-        path: 'dashboard',
-        loadComponent: () =>
-          import('./pages/dashboard/dashboard.component').then(
-            (m) => m.DashboardComponent
-          ),
-      },
-      {
-        path: 'products',
-        loadComponent: () =>
-          import('./pages/products/products.component').then(
-            (m) => m.ProductsComponent
-          ),
-      },
-      {
-        path: 'orders',
-        loadComponent: () =>
-          import('./pages/orders/orders.component').then(
-            (m) => m.OrdersComponent
-          ),
-      },
-      {
-        path: 'users',
-        loadComponent: () =>
-          import('./pages/users/add-user.component').then(
-            (m) => m.AddUserComponent
-          ),
-      },
-      {
-        path: 'settings',
-        loadComponent: () =>
-          import('./pages/settings/settings.component').then(
-            (m) => m.SettingsComponent
-          ),
-      },
-      {
-        path: 'categories',
-        loadComponent: () =>
-          import('./pages/categories/categories.component').then(
-            (m) => m.CategoriesComponent
-          ),
-      },
+      { path: 'dashboard', component: DashboardComponent},
+      { path: 'products', component: ProductsComponent},
+      { path: 'orders', component: OrdersComponent},
+      { path: 'users',component: AddUserComponent},
+      { path: 'settings', component:SettingsComponent},
+      { path: 'categories', component:CategoriesComponent},
     ],
   },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./pages/login/login.component').then((m) => m.LoginComponent),
-  },
+  { path: 'login',component:LoginComponent},
+  { path: '**', component: NotfoundComponent},
 ];
